6a24d907df113b8ba1b917ac3f5bdc0d137c7765
alecasentini <ale.casentini@gmail.com>
2023-05-31 14:38:00 +0200

    Creazione classe e istanze

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
 <?php
+class Movie
+{
+    public $title;
+    public $year;
+    public $genre;
+}
+
+$shining = new Movie();
+$shining->title = "Shining";
+$shining->year = "1980";
+$shining->genre = "horror";
